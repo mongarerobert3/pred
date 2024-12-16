6a7a44d19647888b85c381248f861c5596f8b745
mongarerobert3 <mongarerobert3@gmail.com>
2024-12-16 17:33:33 +0300

    gunicorn

--- Disorder-Prediction System/disease_prediction/settings.py
+++ Disorder-Prediction System/disease_prediction/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'v3v5vfsn0xxjtmb=eoawoiw$5br4g0r&jy_l39995h_93l+-z5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pred-sys.onrender.com']
+ALLOWED_HOSTS = ["pred-cw0f.onrender.com"]
 
 
 # settings.py
