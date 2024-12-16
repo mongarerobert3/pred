6b8c9671c23b3f3ca4e251e72fa0c0e941dc788a
mongarerobert3 <mongarerobert3@gmail.com>
2024-12-16 21:52:54 +0300

    doctor sign_uop

--- Disorder-Prediction System/accounts/views.py
+++ Disorder-Prediction System/accounts/views.py
@@ -175,7 +175,7 @@ def savepdata(request,patientusername):
 
 def signup_doctor(request):
     if request.method == 'GET':
-        return render(request, 'doctor/signup_Form/signup.html')
+        return render(request, 'doctor/signup_form/signup.html')
 
     if request.method == 'POST':
         required_fields = [
