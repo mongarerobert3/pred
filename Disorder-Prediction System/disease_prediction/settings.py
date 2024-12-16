9038d80dfbb311622bd0b0ab517d612c291d09d6
mongarerobert3 <mongarerobert3@gmail.com>
2024-12-16 22:38:27 +0300

    doctor sign_up

--- Disorder-Prediction System/disease_prediction/settings.py
+++ Disorder-Prediction System/disease_prediction/settings.py
@@ -145,6 +145,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 """
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
