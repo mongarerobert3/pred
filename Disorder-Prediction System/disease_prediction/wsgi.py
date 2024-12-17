bf5f29b92d2e45af5c4075033d3633f05377ad42
mongarerobert3 <mongarerobert3@gmail.com>
2024-12-17 10:54:11 +0300

    static file

--- Disorder-Prediction System/disease_prediction/wsgi.py
+++ Disorder-Prediction System/disease_prediction/wsgi.py
@@ -10,11 +10,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disease_prediction.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
